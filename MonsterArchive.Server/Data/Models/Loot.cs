a1078b6bd2d5080e0e1b67da7ac2f9c1fb449a5f
Bryan Castro <bryancastro227@gmail.com>
2025-12-02 23:51:57 -0800

    Entity creation
    Created entities for each table in database. Database is currently being setup

---
+++ MonsterArchive.Server/Data/Models/Loot.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MonsterArchive.Server.Data.Models
+{
+    [Table("Loots")]
+    [Index(nameof(ItemName))]                               // Index on ItemName for faster searches
+    [Index(nameof(Rarity))]                                 // Index on Rarity for filtering
+    [Index(nameof(MonsterId))]                              // Index on MonsterId for join performance
+    public class Loot
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }                         // The unique id and primary key for this Loot
+
+        [Column("itemName")]
+        public required string ItemName { get; set; }       // Name of the loot item
+
+        [Column("rarity")]
+        public required string Rarity { get; set; }         // Rarity of the loot item
+
+        [Column("monsterId")]
+        public int MonsterId { get; set; }                  // Foreign key to the Monster entity
+
+        [ForeignKey(nameof(MonsterId))]
+        public Monster? Monster { get; set; }               // Navigation property to the related Monster
+    }
+}
---
+++ MonsterArchive.Server/Data/Models/Monster.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MonsterArchive.Server.Data.Models
+{
+    [Table("Monsters")]
+    [Index(nameof(Name))]                                                   // Index on Name for faster searches
+    [Index(nameof(Species))]                                                // Index on Species for filtering
+    [Index(nameof(Element))]                                                // Index on Element for filtering
+    [Index(nameof(Weakness))]                                               // Index on Weakness for filtering
+    [Index(nameof(Rank))]                                                   // Index on Rank for filtering
+    [Index(nameof(AggressionLevel))]                                        // Index on AggressionLevel for filtering
+    public class Monster
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }                                         // The unique id and primary key for this City
+
+        [Column("name")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public required string Name { get; set; }                           // Monster name (in UTF8 format)
+
+        [Column("species")]
+        public required string Species { get; set; }                        // Monster species
+
+        [Column("element")]
+        public required string Element { get; set; }                        // Monster elemental type
+
+        [Column("weakness")]
+        public required string Weakness { get; set; }                       // Monster weakness type
+
+        [Column("rank")]
+        public required string Rank { get; set; }                           // Monster rank (e.g., Low, High, M)
+
+        [Column("aggressionLevel")]
+        public required string AggressionLevel { get; set; }                // Monster aggression level
+
+        public ICollection<Loot> Loots { get; set; } = new List<Loot>();    // Navigation property for related Loot items
+    }
+}
